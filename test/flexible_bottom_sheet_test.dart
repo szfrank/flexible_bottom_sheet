23543c027d66b887060e8d6fc0115ba8a0ef0062
Frank <frank.yu@dazong.com>
2018-10-26 10:09:32 +0800

    * update FlexibleBottomSheet

---
+++ test/flexible_bottom_sheet_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flexible_bottom_sheet/flexible_bottom_sheet.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
